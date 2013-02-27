9771e05fbdb53e1e285852f29c271a0d545026fd
sofia <sofia@sofia-virtual-machine.(none)>
2013-02-26 22:29:14 -0800

    client with basic connection to server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Client::Application.config.secret_token = 'dded777b1b210e8285ce47828450c4f40d5df7539b05c428fd305415711206bde1341ffc3cfc69d8fd593125f179ca1161f2dd54c5aadb9de61c7d6d1704cba7'

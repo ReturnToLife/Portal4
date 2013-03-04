cdfff8b08835928ae98fdd71ca68e24df7607c6e
db0company <db0company@gmail.com>
2013-03-04 19:14:53 +0100

    Home page not displayed if not logged in

---
+++ public/design/js/home.js
@@ -0,0 +1,5 @@
+
+// get the current user info.
+// If the user is not logged in, it will display the error that ask the user to log in.
+// Easy one line way to handle authentication :)
+getuser();

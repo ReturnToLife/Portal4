f5ae1b630ef5e3c4da83f8bd14642e90e3da71f7
db0company <db0company@gmail.com>
2013-03-04 18:57:14 +0100

    Error Handling on the usage of the API (show error when not logged in or if the user does not exists)

--- public/design/js/epitech.js
+++ public/design/js/epitech.js
@@ -1,23 +1,33 @@
 
 var intra_url = 'https://intra.epitech.bigint.fr/intra/';
 
-function        default_callback_error(event, request, settings) {
-    // todo: not working with JSONP
-    $("#content").html('<h1>Error</h1><p>' + event + '<br>'
-                      + request + '<br>' + settings + '</p>');
+function        error_shower(type, message, content) {
+    $("#content").html('<h1>Error ' + type + '</h1>'
+		       + '<h3>' + message + '</h3>'
+		       + '<p>' + content + '</p>');
+    console.log("hello");
+    $("content").show();
+}
+
+function        default_callback_error(event, request) {
+    error_shower('404', 'Not Found', 'The page you requested cannot be load. Check if:<ul><li>You\'re logged in the Epitech Intranet ('
+		 + '<a href="' + intra_url + '">' + intra_url
+		 +'</a>)</li><li>The URL is correct</li><li>The information you asked for are valid</li></ul>');
 }
 
 function        wsquery(url, callback, type, callback_error) {
-    a = typeof a !== 'undefined' ? a : 'GET'; // default value
-    a = typeof a !== 'undefined' ? a : default_callback_error; // default value
+    type = typeof type !== 'undefined' ? type : 'GET'; // default value
+    callback_error = typeof callback_error !== 'undefined' ? callback_error : default_callback_error; // default value
 
-    $.ajax({
+    $.jsonp({
             type: type,
             url: intra_url + url + '?callback=?',
             data: { format: 'jsonp'},
             dataType: 'jsonp',
-           }).done(callback);
-    $(document).ajaxError(callback_error);
+	    error: callback_error,
+	    success: callback,
+            done: (function() { $("content").show(); }),
+	    });
 }
 
 function        getuser(login, callback) {

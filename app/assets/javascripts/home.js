b179d9ed7d58f273b254b763f148f28e8231613b
db0company <db0company@gmail.com>
2013-03-26 23:50:18 +0100

    Deactivate epitech login requirement

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,4 +2,4 @@
 // get the current user info.
 // If the user is not logged in, it will display the error that ask the user to log in.
 // Easy one line way to handle authentication :)
-getuser();
+// getuser();
